821702c9a813862187e6cb0fcee4246bc397ef2f
drmustafamansour2018-web <dr.mustafamansour2018@gmail.com>
2026-05-17 04:30:31 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'audit-hh-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-512.png' // تأكد إن اسم صورة الأيقونة الزرقاء عندك كدة بالظبط
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
